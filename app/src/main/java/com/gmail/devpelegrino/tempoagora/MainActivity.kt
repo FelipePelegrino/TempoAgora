00954a5f244f0a0146fe14d0865399654800703c
Felipe Pelegrino <devpelegrino@gmail.com>
2022-05-24 15:25:07 -0300

    feat: add splash screen with androidx.splashscreen

--- app/src/main/java/com/gmail/devpelegrino/tempoagora/MainActivity.kt
+++ app/src/main/java/com/gmail/devpelegrino/tempoagora/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.gmail.devpelegrino.tempoagora
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.gmail.devpelegrino.tempoagora.utils.Constants.SPLASH_SCREEN_DELAY
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSplashScreen(splashScreen = splashScreen)
+    }
+
+    private fun setSplashScreen(splashScreen: SplashScreen) {
+        var keep = true
+        splashScreen.setKeepOnScreenCondition { keep }
+        Handler(Looper.getMainLooper()).postDelayed({ keep = false }, SPLASH_SCREEN_DELAY)
     }
 }
---
+++ app/src/main/java/com/gmail/devpelegrino/tempoagora/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.gmail.devpelegrino.tempoagora.utils
+
+object Constants {
+    const val SPLASH_SCREEN_DELAY = 1500L
+}
